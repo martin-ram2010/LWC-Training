4409516d6111b62c0590ef792defe9511c1ae5d7
martin ram <martin.ram2010@gmail.com>
2020-10-18 21:55:05 +0530

    Day 5

--- src/lwc/contactSlot/contactSlot.js
+++ src/lwc/contactSlot/contactSlot.js
@@ -4,4 +4,11 @@ export default class ContactSlot extends LightningElement {
     handlechanges(event){
         console.log('We\'re in Container Component contactSlot: '+event.target.value);
     }
+    handleClick(event) {
+        this.template.querySelector('div').style.color='green';
+        // this.querySelector('p').style.color='red';
+        this.querySelectorAll('p').forEach(element => {
+            element.style.color='red'; 
+        });
+    }
 }
