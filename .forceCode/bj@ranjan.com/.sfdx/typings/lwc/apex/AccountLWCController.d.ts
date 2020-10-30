e94d56614a03b49bf2a82ca4c491c8076cbef3aa
martin ram <martin.ram2010@gmail.com>
2020-10-30 09:19:02 +0530

    sdf

--- .forceCode/bj@ranjan.com/.sfdx/typings/lwc/apex/AccountLWCController.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "@salesforce/apex/AccountLWCController.getAccountList" {
-  export default function getAccountList(): Promise<any>;
-}
-declare module "@salesforce/apex/AccountLWCController.getContacts" {
-  export default function getContacts(param: {accountId: any}): Promise<any>;
-}
--- .forceCode/bj@ranjan.com/.sfdx/typings/lwc/apex/ContactController.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "@salesforce/apex/ContactController.getContacts" {
-  export default function getContacts(param: {accId: any}): Promise<any>;
-}
-declare module "@salesforce/apex/ContactController.updateContacts" {
-  export default function updateContacts(param: {data: any}): Promise<any>;
-}
